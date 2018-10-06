7440eec60ad24c0438f66cf2692e20549ae8dd3d
rakeshadupa <rakeshadupa931@gmail.com>
2018-10-06 15:21:23 +0530

    d

---
+++ src/com/tvisha/TestCases/LoginTestCase.java
@@ -0,0 +1,47 @@
+package com.tvisha.TestCases;
+
+import java.util.Hashtable;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.tvisha.ExcelData.Ex;
+import com.tvisha.ExcelData.ExcelData;
+import com.tvisha.Pages.LoginPage;
+
+public class LoginTestCase extends BaseTest {
+
+	
+	/*@Test
+	public void login_test() throws InterruptedException{
+		//Log_information.setUpBrowser("chrome");
+		
+		LoginPage loginPage=new LoginPage(driver);
+		loginPage.login_In_page("8331848243", "123465");
+	}*/
+	
+
+	@DataProvider
+	public Object[][] getLoginData(){
+		Ex ex=new Ex();
+		return Ex.getData("C:\\Users\\SatishTvisha\\Desktop\\Book1.xlsx", "Sheet6", null, "YES");
+		/*ExcelData excel=new ExcelData("C:\\Users\\SatishTvisha\\Desktop\\Book1.xlsx");
+		return ExcelData.hashtableGetData("Sheet6","YES");*/
+	}
+	
+	@Test(dataProvider="getLoginData")
+	public void login(Hashtable<String,String> hash_data) throws InterruptedException{
+		LoginPage loginPage=new LoginPage(driver);
+		try{
+		loginPage.login_In_page(hash_data.get("Username"),hash_data.get("Password"));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
